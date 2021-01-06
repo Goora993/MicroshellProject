cc1f73d06eb10951ee53d345b81e994bd3d606ac
Goora993 <ozzyallegropl@gmail.com>
2021-01-06 17:09:56 +0100

    Create app tmp
    - Add few functions
    - Find out what is important to do next

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <unistd.h>
+
+void cd(char PATH[], char COMMAND[]);
+void checkAndDecide(char PATH[], char COMMAND[]);
+void setDefaultPath(char PATH[]);
+
 
 int main() {
-    printf("Hello, World!\n");
+    char USER[] = "Goora993";
+    char PATH[10000];
+    char SIGN = '$';
+    char COMMAND[1000000];
+
+    setDefaultPath(PATH);
+    printf("%s [%s]%c ", USER, PATH, SIGN);
+    gets(COMMAND);
+    checkAndDecide(PATH, COMMAND);
+    printf("%s [%s]%c ", USER, PATH, SIGN);
+
+
     return 0;
+}
+
+void checkAndDecide(char PATH[], char COMMAND[]){
+    if(COMMAND[0]=='c' && COMMAND[1]=='d'){
+        cd(PATH, COMMAND);
+    }
+}
+
+void cd(char PATH[], char COMMAND[]){
+    for (int i = 0; i < 100000; i++) {
+        PATH[i] = COMMAND[i+3];
+    }
+}
+
+void setDefaultPath(char PATH[]){
+    if (getcwd(PATH, 10000) != NULL) {
+
+    } else {
+        perror("getcwd() error");
+    }
 }
