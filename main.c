b784550e4f1fbaae011214739cd2edb59303d4f2
Goora993 <ozzyallegropl@gmail.com>
2021-01-18 19:19:52 +0100

    Add execProcess function
    - execProcess allow to run processes located in PATH (grep, cat etc.)
    - modify cd function - cd without arguments moves to home directory

--- main.c
+++ main.c
@@ -4,31 +4,24 @@
 #include <unistd.h>
 #include <string.h>
 #include <pwd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
+int execProcess(char* command);
 void cd(char *path, char *actualPath, char* pathTmp, const char *command);
-
 void checkAndDecide(char *path, char *actualPath, char* pathTmp, char *command);
-
-void setDefaultPath(char *path);
-
 void setHomePath(char* actualPath);
-
-int ifCommandExit(const char *command);
-
 int checkCommand(const char *command, const char *program);
-
 void ls(const char *actualPath);
-
 void pwd(char *path);
-
 void clearPathTmp(char* pathTmp);
 
-
-
 int BUFFER_SIZE = 10000;
+int LOOP_FLAG = 0;
 char* HOME_PATH;
 
-int main() {
+int main(int argc, char* argv[]) {
 
     char *user = malloc(BUFFER_SIZE * sizeof(char));
     char *path = malloc(BUFFER_SIZE * sizeof(char));
@@ -41,10 +34,9 @@ int main() {
 
     getlogin_r(user, sizeof(user));
 
-
 //app main loop
-    while (ifCommandExit(command) == 0) {
-        printf("%s: %s%c ", user, actualPath, SIGN);
+    while (LOOP_FLAG == 0) {
+        printf("%s[%s] %c ", user, actualPath, SIGN);
         fgets(command, BUFFER_SIZE, stdin); //read command from stdin
         command[strcspn(command, "\n")] = 0; //remove \n sign after line with command
         checkAndDecide(path, actualPath, pathTmp, command);
@@ -59,35 +51,67 @@ int main() {
     return 0;
 }
 
+
+
+
 void checkAndDecide(char *path, char *actualPath, char* pathTmp, char *command) {
     if (checkCommand(command, "cd") != -1) {
         cd(path, actualPath, pathTmp, command);
     } else if (checkCommand(command, "ls") != -1) {
         ls(actualPath);
     } else if (checkCommand(command, "pwd") != -1) {
-        pwd(path);
+        pwd(actualPath);
+    } else if (checkCommand(command, "exit") != -1) {
+        LOOP_FLAG = -1;
+    } else {
+        execProcess(command);
     }
 }
 
 
-void setHomePath(char* actualPath) {
-    if((HOME_PATH = getenv("HOME")) == NULL) {
-        HOME_PATH = getpwuid(getuid())->pw_dir;
+int execProcess(char* command) {
+    pid_t pid = fork();
+    if(pid<0){
+        perror("fork failed");
+        exit(1);
     }
+    else if(pid==0){
+        char d[4] = " ";
+        int counter = 0;
 
-    chdir(HOME_PATH);
 
-    int homePathLength = strlen(HOME_PATH);
+        command[strcspn(command, "\n")] = 0;
 
+        char *inputPart = strtok(command, d);
+        char* args[1024];
 
-    for (int i = 0; i < homePathLength; ++i) {
-        actualPath[i] = HOME_PATH[i];
+        if(inputPart == NULL){
+            args[counter] = command;
+        }
+
+        while(inputPart != NULL)
+        {
+            args[counter] = inputPart;
+            args[counter+1] = NULL;
+            inputPart = strtok(NULL, d);
+            counter++;
+        }
+
+        counter = 0;
+
+        execvp(args[0], args);
+        printf("Error number: %d", errno);
     }
+    // parent
+
+    wait(NULL);
+    return 0;
 }
 
 
 void cd(char *path, char *actualPath, char* pathTmp, const char *command) {
     int loopFlag = 0;
+    int pathLength = 0;
     getcwd(actualPath, BUFFER_SIZE);
 
     for (int i = 0; i < BUFFER_SIZE; i++) {
@@ -95,12 +119,18 @@ void cd(char *path, char *actualPath, char* pathTmp, const char *command) {
         pathTmp[i] = actualPath[i];
     }
 
-    DIR *localPath = opendir(path);
+    pathLength = strlen(path);
 
-    if (localPath != NULL) {
+
+    DIR *localPath = opendir(path);
+    if(pathLength==0){
+        strncpy(actualPath, HOME_PATH, BUFFER_SIZE);
+        chdir(actualPath);
+        printf("Actual path: %s\n", actualPath);
+    } else if (localPath != NULL) {
         if (path[0] == '/') {
             strncpy(actualPath, path, BUFFER_SIZE);
-            chdir(path);
+            chdir(actualPath);
         } else if(path[0] == '.' && path[1] != '.'){
             if(path[1] == '/'){
                 strncpy(pathTmp, path+1, BUFFER_SIZE-1);
@@ -116,9 +146,6 @@ void cd(char *path, char *actualPath, char* pathTmp, const char *command) {
                     loopFlag = 1;
                 }
             }
-            if(actualPath[1]==':' && actualPath[2]==0){
-                strcat(actualPath, "/");
-            }
             chdir(actualPath);
         } else {
             strcat(actualPath, "/");
@@ -128,6 +155,16 @@ void cd(char *path, char *actualPath, char* pathTmp, const char *command) {
     } else {
         printf("Brak takiej scizeki\n");
     }
+}
+
+void setHomePath(char* actualPath) {
+    if((HOME_PATH = getenv("HOME")) == NULL) {
+        HOME_PATH = getpwuid(getuid())->pw_dir;
+    }
+
+    chdir(HOME_PATH);
+
+    strncpy(actualPath, HOME_PATH, BUFFER_SIZE);
 }
 
 void ls(const char *actualPath) {
@@ -140,15 +177,13 @@ void ls(const char *actualPath) {
 
         closedir(localPath);
     } else
-        printf("! wywołujac funkcje opendir(%s) pojawil sie blad otwarcia strumienia dla danej sciezki, moze nie istnieje, "
-               "lub podano sciezkę pusta\n", actualPath);
+        printf("Podano bledna sciezke: %s\n", actualPath);
 
 }
 
-void pwd(char *path) {
-    printf("%s\n", path);
+void pwd(char *actualPath) {
+    printf("%s\n", actualPath);
 }
-
 
 int checkCommand(const char *command, const char *program) {
     for (int i = 0; program[i]; i++) {
@@ -156,14 +191,6 @@ int checkCommand(const char *command, const char *program) {
             return -1;
     }
     return 0;
-}
-
-int ifCommandExit(const char *command) {
-    if (command[0] == 'e' && command[1] == 'x' && command[2] == 'i' && command[3] == 't') {
-        return 1;
-    } else {
-        return 0;
-    }
 }
 
 void clearPathTmp(char* pathTmp){
